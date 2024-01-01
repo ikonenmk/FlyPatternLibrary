3c03d3cf226208f5876c5d2c3ca16b12d9bad14a
ikonenmk <markus.ikonen.karlstrom@gmail.com>
2024-01-01 15:01:44 +0100

    Endpoints for adding and finding user by id done

--- src/main/java/com/example/flypatternlib/controller/UserController.java
+++ src/main/java/com/example/flypatternlib/controller/UserController.java
@@ -4,6 +4,10 @@ import com.example.flypatternlib.model.User;
 import com.example.flypatternlib.repository.UserRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/user")
@@ -19,5 +23,18 @@ public class UserController {
     @PostMapping
     public void add(@RequestBody User user) {
         repository.save(user);
+    }
+
+    //Find user by id
+    @GetMapping("/{user_id}")
+    public Optional<User> findById(@PathVariable Integer user_id) {
+
+        //Search for user id, return only if Optional<User> is non-empty
+        Optional<User> foundUser = repository.findById(user_id);
+        if (foundUser.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found.");
+        } else {
+            return foundUser;
+        }
     }
 }
