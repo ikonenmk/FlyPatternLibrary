450bb51f606757a4fc0dfe166bdf2faef6d698cb
ikonenmk <markus.ikonen.karlstrom@gmail.com>
2024-01-12 06:23:22 +0100

    SpeciesController klart för nu MaterialControler klart för nu

--- src/main/java/com/example/flypatternlib/controller/MaterialController.java
+++ src/main/java/com/example/flypatternlib/controller/MaterialController.java
@@ -1,9 +1,43 @@
 package com.example.flypatternlib.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.flypatternlib.model.Material;
+import com.example.flypatternlib.repository.MaterialRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/material")
 public class MaterialController {
+    private final MaterialRepository repository;
+
+    public MaterialController(MaterialRepository repository) {
+        this.repository = repository;
+    }
+
+    //Find all materials in db
+    @GetMapping
+    public List<Material> findAll() {
+        return repository.findAll();
+    }
+
+    //Add a new material
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public void add(@RequestBody Material material) {
+        repository.save(material);
+    }
+
+    //Delete material
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{material_id}")
+    public void delete(@PathVariable Integer material_id) {
+        //throw error if species id not found
+        if(!repository.existsById(material_id)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Species not found.");
+        }
+        repository.deleteById(material_id);
+    }
 }
